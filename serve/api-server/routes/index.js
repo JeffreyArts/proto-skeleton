0c3f330d8fd496bdf39d6cd2f566d2869fc6723f
JeffreyArts <sjeffff@gmail.com>
2017-05-31 09:53:25 +0200

    Apply isSelf to DELETE::/accounts/:accountId

--- serve/api-server/routes/index.js
+++ serve/api-server/routes/index.js
@@ -13,11 +13,11 @@ module.exports = function(app) {
     // Home
     app.get("/"                                                                     , requireApi("controllers/home"));
     // Accounts / Authorization
-    app.delete("/accounts/:accountId"                                               , requireApi("controllers/account/delete"));
     app.post("/accounts"                                                            , requireApi("controllers/account/create"));
-    app.patch("/accounts/:accountId"    , isAuthorized, isSelf                      , requireApi("controllers/account/update"));
     app.post("/register"                                                            , requireApi("controllers/account/create"));
     app.get("/auth"                     , isAuthorized                              , requireApi("controllers/auth/me"));
+    app.delete("/accounts/:accountId"   , isAuthorized, isSelf                      , requireApi("controllers/account/delete"));
+    app.patch("/accounts/:accountId"    , isAuthorized, isSelf                      , requireApi("controllers/account/update"));
 
 
     // Local auth
